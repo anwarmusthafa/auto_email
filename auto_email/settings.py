3702c342c3c75a04e505451fa3ce4e7c5a1f8e86
unknown <anwarmusthafa99@gmail.com>
2024-12-06 02:25:51 +0530

    .gitignore and .env added

--- auto_email/settings.py
+++ auto_email/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n0@h94&%#wm0x(z^9#t9dq100ws9t@qr@$sr1a$s-8=79a&_!)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
