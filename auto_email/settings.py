3b4b4e930d84265c06bc813005af7a7cd61b4653
unknown <anwarmusthafa99@gmail.com>
2024-12-07 21:30:11 +0530

    refact: settings.py change

--- auto_email/settings.py
+++ auto_email/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='very-long-secrey-key')
+SECRET_KEY ='django-insecure-k%#c@!=we%3%nq)ij4#q58j6b(tdb&o%pyw5c_cw$y(eyrvh9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool) 
